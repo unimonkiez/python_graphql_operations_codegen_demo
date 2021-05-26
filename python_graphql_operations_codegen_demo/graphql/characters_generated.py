ae281fbfd6fa598a5e82a15b3404c0727111f3f8
Yuval Saraf <unimonkiez@gmail.com>
2021-05-26 03:49:44 +0300

    Tested parameters, also good

--- python_graphql_operations_codegen_demo/graphql/characters_generated.py
+++ python_graphql_operations_codegen_demo/graphql/characters_generated.py
@@ -88,3 +88,57 @@ async def execute_async_get_characters_gql() -> GetCharactersResponse:
   )
   response_dict = await response_text_promise
   return from_dict(data_class=GetCharactersResponse, data=response_dict)
+
+
+_gql_get_character_gql = gql("""
+query get_character($id: ID!) {
+  character(id: $id) {
+    name
+  }
+}
+
+""")
+
+
+@dataclass
+class GetCharacterResponse:
+
+  @dataclass
+  class CharacterSelection:
+  
+    name: str
+    
+  
+  __GQL_CODEGEN_CharacterSelection__ = CharacterSelection
+  
+  character: CharacterSelection
+  
+
+__GQL_CODEGEN_GetCharacterResponse__ = GetCharacterResponse
+
+
+
+def execute_get_character_gql(id: str) -> GetCharacterResponse:
+  client = _get_client_sync()
+
+  response_dict = client.execute_sync(
+    _gql_get_character_gql,
+    variable_values={
+      "id": id,
+    },
+  )
+  return from_dict(data_class=GetCharacterResponse, data=response_dict)
+
+
+
+async def execute_async_get_character_gql(id: str) -> GetCharacterResponse:
+  client = _get_client_async ()
+
+  response_text_promise = client.execute_async(
+    _gql_get_character_gql,
+    variable_values={
+      "id": id,
+    },
+  )
+  response_dict = await response_text_promise
+  return from_dict(data_class=GetCharacterResponse, data=response_dict)
--- python_graphql_operations_codegen_demo/start.py
+++ python_graphql_operations_codegen_demo/start.py
@@ -1,4 +1,4 @@
-from .graphql.characters_generated import execute_async_get_characters_gql, execute_get_characters_gql
+from .graphql.characters_generated import execute_async_get_characters_gql, execute_get_character_gql
 import asyncio
 
 async def start_async() -> None:
@@ -6,8 +6,8 @@ async def start_async() -> None:
     print(res.characters.results[1].name)
 
 def start_sync() -> None:
-    res = execute_get_characters_gql()
-    print(res.characters.results[0].name)
+    res = execute_get_character_gql(id="6")
+    print(res.character.name)
 
 def start() -> None:
     start_sync()
